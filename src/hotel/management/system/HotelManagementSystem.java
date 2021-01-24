67eff7bc6a59ef0f180a6ac979e6b376d587b3a0
rohanmittal <rohanmittal@gmail.com>
2021-01-25 00:06:09 +0530

    initial commit

---
+++ src/hotel/management/system/HotelManagementSystem.java
@@ -0,0 +1,55 @@
+package hotel.management.system;
+
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.*;
+
+public class HotelManagementSystem extends JFrame {
+
+    HotelManagementSystem() {
+
+        setBounds(100, 100, 1300, 665);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("hotel/management/system/icons/download.jpg"));
+        JLabel jl = new JLabel(i1);
+
+        jl.setBounds(0, 0, 1300, 665);
+        add(jl);
+
+        JLabel jl2 = new JLabel("Hotel Management System");
+        jl2.setBounds(300, 10, 1000, 70);
+        jl2.setForeground(Color.GREEN);
+        jl2.setFont(new Font("serif", Font.PLAIN, 70));
+        jl.add(jl2);
+
+        JButton jb1 = new JButton("Next");
+        jb1.setBackground(Color.white);
+        jb1.setForeground(Color.black);
+        jb1.setBounds(1100, 560, 120, 30);
+        jl.add(jb1);
+        
+        
+        
+        setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        while (true) {
+            jl2.setVisible(false);
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+            }
+            jl2.setVisible(true);
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        new HotelManagementSystem();
+    }
+
+}
---
+++ src/hotel/management/system/login.java
@@ -0,0 +1,76 @@
+
+
+package hotel.management.system;
+
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+
+public class login extends JFrame{
+    
+    JLabel jl1,jl2;
+    JTextField jtf1;
+    JPasswordField jpf1;
+    JButton jb1,jb2;
+    
+    
+    
+    login(){
+    
+        jl1 = new JLabel("Username");
+        jl1.setBounds(40,20,100,30);
+        add(jl1);
+        
+        jl2 = new JLabel("Password");
+        jl2.setBounds(40,70,100,30);
+        add(jl2);
+        
+        jtf1 = new JTextField();
+        jtf1.setBounds(150,20,150,30);
+        add(jtf1);
+        
+        jpf1 = new JPasswordField();
+        jpf1.setBounds(150,70,150,30);
+        jpf1.setEchoChar('*');
+        add(jpf1);
+        
+        jb1 = new JButton("Login");
+        jb1.setBackground(Color.white);
+        jb1.setForeground(Color.black);
+        jb1.setBounds(40, 150, 120, 30);
+        add(jb1);
+        
+        jb2 = new JButton("Cancel");
+        jb2.setBackground(Color.white);
+        jb2.setForeground(Color.black);
+        jb2.setBounds(180, 150, 120, 30);
+        add(jb2);
+                
+        /*ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("C:\\Users\\asus\\Documents\\NetBeansProjects\\Hotel Management System\\src\\hotel\\management\\system\\icons\\login.png"));
+        JLabel jl3 = new JLabel();
+        jl3.setBounds(350,10,200,200);
+        add(jl3);
+        add(i1);*/
+        
+        
+        setLayout(null);
+        setBounds(550,350,600,400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        
+}
+        public static void main(String args[])
+        {
+            new login();
+        }
+
+    private void add(ImageIcon i1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
